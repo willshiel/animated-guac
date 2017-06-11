67e5cd83077120ce6b7e9b4d335faf6575999169
willshiel <will@will.fios-router.home>
2017-06-11 11:44:06 -0400

    Continuing through the demo

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='polls/', permanent=False), name='index')
 ]
