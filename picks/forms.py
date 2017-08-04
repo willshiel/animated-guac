8425c55988e98a623217f40b35092da22b2814a6
Will Shiel <ws248531@muhlenberg.edu>
2017-08-04 07:13:30 -0400

    fixed picks bug

--- picks/forms.py
+++ picks/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import BaseFormSet, ValidationError
 from .models import Pick, Game, Team
 from django.contrib.auth.models import User
 from common.current_week import CURRENT_WEEK
@@ -9,12 +10,25 @@ class PickForm(forms.Form):
         A form that allows a user to make a pick on the
         selected game
     '''
-    team_picked = forms.CharField(max_length=50)
+
+    team_picked = forms.IntegerField()
 
     def save(self, user):
-        pdb.set_trace()
         cd = self.cleaned_data
         team_picked = Team.objects.get(pk=cd['team_picked'])
         pick = Pick(team_picked=team_picked, week=CURRENT_WEEK, user=user)
         pick.save()
 
+
+class BasePickFormSet(BaseFormSet):
+    '''
+        Overrides the save method for custom validations
+    '''
+
+    def clean(self):
+        forms = self.forms
+        for form in forms:
+            try:
+                is_team_picked = form.cleaned_data['team_picked']
+            except (KeyError):
+                raise ValidationError("You must select a team for each matchup")
--- picks/views.py
+++ picks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Team, Game, Pick
 from home.models import Profile
-from .forms import PickForm
+from .forms import PickForm, BasePickFormSet
 from django.forms import formset_factory
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
@@ -9,6 +9,7 @@ import pdb
 from django import forms
 from django.contrib.auth.decorators import login_required
 from common.current_week import CURRENT_WEEK
+from django.core.exceptions import ValidationError
 
 @login_required(redirect_field_name='') # required to login to get to this page
 def get_picks(request):
@@ -17,22 +18,31 @@ def get_picks(request):
     if profile.has_picked:
         return render(request, 'picks/alreadypicked.html')
     games = Game.objects.filter(week=CURRENT_WEEK)
-    amount_of_games = len(games)
-    PickFormSet = formset_factory(PickForm, extra=amount_of_games)
+    PickFormSet = formset_factory(PickForm, formset=BasePickFormSet, extra=len(games))
     if request.method == 'POST':
         formset = PickFormSet(request.POST, request.FILES)
         if formset.is_valid():
             for form in formset:
-                pdb.set_trace()
                 form.save(request.user)
             profile.has_picked = True
-            # profile.save()
+            profile.save()
             return HttpResponseRedirect('/home/')
+        else:
+            create_team_picked_field(formset, games)
+
     else:
         formset = PickFormSet()
-        for i in range(0, amount_of_games):
-            formset[i].fields['team_picked'] = forms.ModelChoiceField(
-                queryset=Team.objects.filter(name=games[i].home_team_name) | Team.objects.filter(name=games[i].away_team_name)
-            )
+        create_team_picked_field(formset, games)
 
     return render(request, 'picks/picks.html', { 'formset': formset, 'games': games, 'range': range(2) })
+
+
+"""
+    creates the fields for selecting a team
+"""
+def create_team_picked_field(formset, games):
+    for i in range(0, len(games)):
+        formset[i].fields['team_picked'] = forms.ModelChoiceField(
+            queryset=Team.objects.filter(name=games[i].home_team_name) | Team.objects.filter(
+                name=games[i].away_team_name)
+        )
