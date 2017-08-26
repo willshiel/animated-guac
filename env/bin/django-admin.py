57fc4e11d9cab4d7e1b27531c142ad0d3d99da36
willshiel <ws248531@muhlenberg.edu>
2017-08-25 20:49:37 -0400

    Set up virtual environment

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/will/Development/heroku-python/python-getting-started/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
